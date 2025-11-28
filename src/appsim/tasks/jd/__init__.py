774998fc7ea8188e51f6b3b5450fe0e40ca52afd
ddddddjy <3622495956@qq.com>
2025-11-28 09:36:57 +0800

    feat:JD任务动态验证

--- src/appsim/tasks/jd/__init__.py
+++ src/appsim/tasks/jd/__init__.py
@@ -30,12 +30,6 @@ from .eval_28 import validate_task_twenty_eight
 from .eval_29 import validate_task_twenty_nine
 from .eval_30 import validate_task_thirty
 from .eval_31 import validate_task_thirty_one
-from .eval_32 import validate_task_thirty_two
-from .eval_33 import validate_task_thirty_three
-from .eval_34 import validate_task_thirty_four
-from .eval_35 import validate_task_thirty_five
-from .eval_36 import validate_task_thirty_six
-from .eval_37 import validate_task_thirty_seven
 
 JD_TASKS = AppTasks(
     package_name="com.example.MyJD",
@@ -71,13 +65,13 @@ JD_TASKS = AppTasks(
             is_reasoning=True,
         ),
         TaskItem(
-            instruction="结算我的第一个待付款订单后再确认收货。",
+            instruction="结算我的第一个待付款订单。",
             verify_func=validate_task_six,
-            human_steps=6,
+            human_steps=3,
             is_reasoning=False,
         ),
         TaskItem(
-            instruction="给Apple官方旗舰店发消息问手机什么时候发货。",
+            instruction="给Apple产品京东自营旗舰店发消息问什么时候发货。",
             verify_func=validate_task_seven,
             human_steps=5,
             is_reasoning=False,
@@ -113,15 +107,15 @@ JD_TASKS = AppTasks(
             is_reasoning=False,
         ),
         TaskItem(
-            instruction="算一下首页全部商品中，评分大于等于4.7的有几个，给出一个阿拉伯数字即可。",
+            instruction="算一下首页前十个商品中，评分大于等于4.7的有几个，给出一个阿拉伯数字即可。",
             verify_func=validate_task_thirteen,
-            human_steps=6,
+            human_steps=3,
             is_reasoning=True,
         ),
         TaskItem(
-            instruction="查看首页华为商品评论数最多的为多少条,给出一个阿拉伯数字即可。",
+            instruction="查看首页iPhone15商品共有多少条评论,给出一个阿拉伯数字即可。",
             verify_func=validate_task_fourteen,
-            human_steps=8,
+            human_steps=4,
             is_reasoning=False,
         ),
         TaskItem(
@@ -155,7 +149,7 @@ JD_TASKS = AppTasks(
             is_reasoning=False,
         ),
         TaskItem(
-            instruction="设置Apple官方旗舰店的聊天为消息免打扰。",
+            instruction="设置Apple产品京东自营旗舰店的聊天为消息免打扰。",
             verify_func=validate_task_twenty,
             human_steps=4,
             is_reasoning=False,
@@ -173,7 +167,7 @@ JD_TASKS = AppTasks(
             is_reasoning=False,
         ),
         TaskItem(
-            instruction="计算Apple官方旗舰店评分大于4.7的商品的平均价格，保留一位小数。",
+            instruction="计算Apple产品京东自营旗舰店评分大于4.7的商品的平均价格，保留一位小数。",
             verify_func=validate_task_twenty_three,
             human_steps=6,
             is_reasoning=True,
@@ -215,7 +209,7 @@ JD_TASKS = AppTasks(
             is_reasoning=True,
         ),
         TaskItem(
-            instruction="进入Apple官方旗舰店选择价格最高的商品规格加入购物车并结算，选择赵六的地址。",
+            instruction="进入Apple产品京东自营旗舰店选择价格最高的商品规格加入购物车并结算，选择赵六的地址。",
             verify_func=validate_task_thirty,
             human_steps=15,
             is_reasoning=True,
@@ -224,42 +218,6 @@ JD_TASKS = AppTasks(
             instruction="查看购物车中所有商品，将价格最高的三件商品移出购物车。",
             verify_func=validate_task_thirty_one,
             human_steps=12,
-            is_reasoning=True,
-        ),
-        TaskItem(
-            instruction="找到首页中价格最低的手机，选择其最便宜的规格购买。",
-            verify_func=validate_task_thirty_two,
-            human_steps=7,
-            is_reasoning=True,
-        ),
-        TaskItem(
-            instruction="比较Apple官方旗舰店和华为官方旗舰店的粉丝数，告诉我粉丝量更高的店铺名。",
-            verify_func=validate_task_thirty_three,
-            human_steps=8,
-            is_reasoning=True,
-        ),
-        TaskItem(
-            instruction="将陈七地址详情中的电话号码改成18972746987。",
-            verify_func=validate_task_thirty_four,
-            human_steps=7,
-            is_reasoning=False,
-        ),
-        TaskItem(
-            instruction="进入华为官方旗舰店选择评分最高的商品加入购物车并结算，选择李四的地址。",
-            verify_func=validate_task_thirty_five,
-            human_steps=12,
-            is_reasoning=True,
-        ),
-        TaskItem(
-            instruction="将首页所有华为手机商品的最小的内存版本加入购物车。",
-            verify_func=validate_task_thirty_six,
-            human_steps=14,
-            is_reasoning=True,
-        ),
-        TaskItem(
-            instruction="购买首页华为商品中买家评价最多的商品。",
-            verify_func=validate_task_thirty_seven,
-            human_steps=15,
             is_reasoning=True,
         ),
     ],
