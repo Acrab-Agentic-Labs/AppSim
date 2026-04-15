da580a95764af4afc568db3ea6304c871a046e44
lcl <3150684365@qq.com>
2026-04-15 23:51:42 +0800

    feat: add Zoom and Booking tasks

--- src/appsim/tasks/__init__.py
+++ src/appsim/tasks/__init__.py
@@ -10,11 +10,14 @@ from .jd import JD_TASKS
 from .music import MUSIC_TASKS
 from .rednote import RED_NOTE_TASKS
 from .tencentmeeting import TENCENT_MEETING_TASKS
+from .zoom import ZOOM_TASKS
+from .booking import BOOKING_TASKS
 from .wechat import WECHAT_TASKS
 
 
 class AppEnum(Enum):
     BILIBILI = "Bilibili"
+    BOOKING = "Booking"
     CTRIP = "Ctrip"
     ELEME = "EleMe"
     GAODE = "Gaode"
@@ -22,11 +25,13 @@ class AppEnum(Enum):
     MYJD = "MyJD"
     RED_NOTE = "RedNote"
     TENCENT_MEETING = "TencentMeeting"
+    ZOOM = "Zoom"
     WECHAT = "Wechat"
 
 
 APP_TASKS_MAP = {
     AppEnum.BILIBILI: BILIBILI_TASKS,
+    AppEnum.BOOKING: BOOKING_TASKS,
     AppEnum.CTRIP: CTRIP_TASKS,
     AppEnum.ELEME: ELEME_TASKS,
     AppEnum.GAODE: GAODE_TASKS,
@@ -34,5 +39,6 @@ APP_TASKS_MAP = {
     AppEnum.MYJD: JD_TASKS,
     AppEnum.RED_NOTE: RED_NOTE_TASKS,
     AppEnum.TENCENT_MEETING: TENCENT_MEETING_TASKS,
+    AppEnum.ZOOM: ZOOM_TASKS,
     AppEnum.WECHAT: WECHAT_TASKS,
 }
